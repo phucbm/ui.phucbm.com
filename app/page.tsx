f4c548c37b1a7550bb877ed747dbe9eb5feb5f54
phucbm <hiphucbui@gmail.com>
2025-10-12 21:53:46 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
   return (
     <div className="max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8">
       <header className="flex flex-col gap-1">
-        <h1 className="text-3xl font-bold tracking-tight">Custom Registry</h1>
+        <h1 className="text-3xl font-bold tracking-tight">
+            Perxel UI
+        </h1>
         <p className="text-muted-foreground">
           A custom registry for distributing code using shadcn.
         </p>
