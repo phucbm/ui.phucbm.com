cbb956ea8764747eb2152c6a129107c906e40bf4
vandangnhathung <vandangnhathung@gmail.com>
2025-10-14 01:32:49 +0700

    chore: update value

--- registry/perxel/blocks/slide-carousel/page.tsx
+++ registry/perxel/blocks/slide-carousel/page.tsx
@@ -2,12 +2,6 @@ import SlideCarousel from "./components/slide-carousel";
 
 export default async function Page() {
   return (
-    <SlideCarousel
-      baseSpeed={0.5}
-      hoverSlowdownRatio={0.5}
-      autoSlide={true}
-      infinite={true}
-      manualNav={false}
-    />
+    <SlideCarousel/>
   );
 }
