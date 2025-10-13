fb7fd131d84d442a02ffae61ecbbe2f40c60492e
phucbm <hiphucbui@gmail.com>
2025-10-13 16:09:07 +0700

    refactor(setup): add mdx support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        useCache: true,
+        globalNotFound: true,
+    },
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+    // Add markdown plugins here, as desired
+})
+
+export default withMDX(nextConfig)
