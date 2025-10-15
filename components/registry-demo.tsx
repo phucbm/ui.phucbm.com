349d91a296393dce552a781dad14047ebf4b15d4
phucbm <hiphucbui@gmail.com>
2025-10-16 00:19:34 +0700

    feat(registry): add fullScreenDemo

--- components/registry-demo.tsx
+++ components/registry-demo.tsx
@@ -1,7 +1,7 @@
 import {getRegistryItem} from "@/lib/getRegistryItem";
 import {RegistryPreview} from "@/components/registry-preview";
 import React from "react";
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import {MaximizeRegistry} from "@/components/maximizable-registry";
 import {getRegistryCodeItems} from "@/lib/getRegistryCodeItems";
 import CodeBlockView, {CodeItem} from "@/components/code-block-view";
@@ -12,9 +12,17 @@ type Props = {
     code?: CodeItem[];
     hashId?: string; // for maximizable registry
     subtitle?: string; // for maximizable registry
+    fullScreenDemo?: React.ReactNode; // new prop
 };
 
-export async function RegistryDemo({children, name, code, hashId, subtitle}: Props) {
+export async function RegistryDemo({
+                                       children,
+                                       name,
+                                       code,
+                                       hashId,
+                                       subtitle,
+                                       fullScreenDemo,
+                                   }: Props) {
     const registryItem = await getRegistryItem(name);
     const hasFiles = registryItem.files.length > 0;
 
@@ -23,40 +31,39 @@ export async function RegistryDemo({children, name, code, hashId, subtitle}: Pro
         code = await getRegistryCodeItems({registryItem});
     }
 
+    // fallback to children if fullScreenDemo not provided
+    const demoForFullscreen = fullScreenDemo ?? children;
+
     return (
-        <>
-            <Tabs defaultValue="preview" className="pt-6">
-
-                {/*demo header*/}
-                <div className="flex justify-between items-center">
-                    <TabsList>
-                        <TabsTrigger value="preview">Preview</TabsTrigger>
-                        {hasFiles && <TabsTrigger value="code">Code</TabsTrigger>}
-                    </TabsList>
-                    <div>
-                        <MaximizeRegistry children={children} registryItem={registryItem} code={code} hashId={hashId}
-                                          subtitle={subtitle}/>
-                    </div>
-                </div>
-
-                {/*preview*/}
-                <TabsContent value="preview" className="">
-                    <RegistryPreview
-                        children={children}
+        <Tabs defaultValue="preview" className="pt-6">
+            {/* demo header */}
+            <div className="flex justify-between items-center">
+                <TabsList>
+                    <TabsTrigger value="preview">Preview</TabsTrigger>
+                    {hasFiles && <TabsTrigger value="code">Code</TabsTrigger>}
+                </TabsList>
+                <div>
+                    <MaximizeRegistry
+                        children={demoForFullscreen}
                         registryItem={registryItem}
+                        code={code}
+                        hashId={hashId}
+                        subtitle={subtitle}
                     />
+                </div>
+            </div>
+
+            {/* preview */}
+            <TabsContent value="preview">
+                <RegistryPreview children={children} registryItem={registryItem}/>
+            </TabsContent>
+
+            {/* code */}
+            {hasFiles && (
+                <TabsContent value="code">
+                    <CodeBlockView code={code}/>
                 </TabsContent>
-
-                {/*code*/}
-                {hasFiles &&
-                    <TabsContent value="code">
-                        <CodeBlockView code={code}/>
-                    </TabsContent>
-                }
-
-            </Tabs>
-
-
-        </>
+            )}
+        </Tabs>
     );
 }
