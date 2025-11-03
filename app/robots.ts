7e128fdea20a55f462d42f75614916a3328f7cc9
phucbm <hiphucbui@gmail.com>
2025-11-03 23:49:19 +0700

    feat(seo): add sitemap.ts, update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,13 @@
-// app/robots.ts
-import type {MetadataRoute} from "next";
+import type {MetadataRoute} from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const comingSoon = process.env.SHOW_COMING_SOON?.toLowerCase() === "true";
-
-    return comingSoon
-        ? {
-            rules: [{userAgent: "*", disallow: "/"}],
-        }
-        : {
-            rules: [{userAgent: "*", allow: "/"}],
-            sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
-        };
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://yoursite.com'
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: '/private/',
+        },
+        sitemap: `${baseUrl}/sitemap.xml`,
+    }
 }
