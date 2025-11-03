dbbe96cd0a7cdc5d3718dfc6005bdbea02f962c3
phucbm <hiphucbui@gmail.com>
2025-11-03 23:44:23 +0700

    feat(seo): add umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import 'nextra-theme-docs/style.css'
+import {Head} from 'nextra/components'
 import 'app/globals.css'
 import {generatePageMetadata} from "@phucbm/next-og-image";
 import {metadata} from "@/lib/seo";
@@ -18,7 +18,12 @@ export default async function RootLayout({children}) {
             dir="ltr"
             // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
             suppressHydrationWarning
+            style={{['--nextra-content-width' as any]: 'auto'}}
         >
+        <Head>
+            <script defer src="https://cloud.umami.is/script.js"
+                    data-website-id="04f28117-bf34-4aa8-8db0-d1a26122aad2"></script>
+        </Head>
         <body>
         {children}
         </body>
