a820d296e71df17d5b7933764332cfa2f66ee96b
phucbm <hiphucbui@gmail.com>
2025-11-07 00:54:41 +0700

    feat(registry): update doc

--- registry/phucbm/blocks/infinite-image-carousel/example.tsx
+++ registry/phucbm/blocks/infinite-image-carousel/example.tsx
@@ -3,24 +3,25 @@ import {InfiniteImageCarousel} from "@/registry/phucbm/blocks/infinite-image-car
 export default function Example() {
     return (
         <div className="h-screen flex flex-col justify-around py-[10vh]">
-
+            {/*logos*/}
             <InfiniteImageCarousel
                 itemClass="lg:mr-8 lg:px-4 px-2 xl:[--width:12vw] lg:[--width:150px] [--width:100px]"
                 isLogo={true}
                 direction={1}
-                images={exampleLogoImages()}/>
+                images={exampleLogos()}
+            />
 
-            <InfiniteImageCarousel images={exampleImages()}/>
-
+            {/*photos*/}
+            <InfiniteImageCarousel images={examplePhotos()}/>
         </div>
     );
 }
 
-const exampleImages = () => Array.from({length: 10}, (_, i) => ({
+const examplePhotos = () => Array.from({length: 10}, (_, i) => ({
     url: `https://picsum.photos/400/400?random=${Math.random()}`,
     title: `Image ${i + 1}`
 }));
-const exampleLogoImages = () => {
+const exampleLogos = () => {
     const brands = ['apple', 'google', 'microsoft', 'meta', 'netflix', 'spotify', 'samsung', 'intel', 'adobe'];
     const shuffled = brands.sort(() => Math.random() - 0.5);
     return shuffled.map((brand, i) => ({
